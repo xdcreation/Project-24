a6fba4ca56aaa496b3296db46dc1d73de0cca48d
xdcreation <69704397+xdcreation@users.noreply.github.com>
2021-11-09 19:21:57 -0600

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,27 @@
+class Iron {
+    constructor(x, y) {
+      var options = {
+        'density':30,
+        'friction': 3,
+        'restitution':0.8
+      };
+      this.body = Bodies.rectangle(x, y, 50, 50, options);
+      this.width = 80;
+      this.height = 50;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('brown')
+      fill('brown')
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
